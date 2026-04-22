ee45abc7ec6874bd648404a9c957a7f6c8d41ab9
basant <mahatbasant414@gmail.com>
2026-04-23 00:58:19 +0545

    change server

--- src/api/apiClient.js
+++ src/api/apiClient.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || 'https://capitalitsolution-backend-production.up.railway.app',
+  baseURL: import.meta.env.VITE_API_BASE_URL + `/api` || 'https://capitalitsolution-backend-production.up.railway.app/api',
   headers: {
     'Content-Type': 'application/json'
   }
